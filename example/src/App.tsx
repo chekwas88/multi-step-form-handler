933ccb05a26cc684aa2275c0d174fa4d42a91eec
chisom <princechekwas@gmail.com>
2021-10-25 10:06:19 +0100

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'multi-step-form-handler'
+import 'multi-step-form-handler/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
